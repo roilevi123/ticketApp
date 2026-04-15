bde6aff5dc1a7f5be6fbc13a57527ff0e6761d50
roilevi05 <  -GitHub>
2026-04-15 08:15:21 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import Domain.Company.Company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Company company = new Company("1","1");
+//        Company company = new Company("1","1");
 
     }
 }
