f50874e11849fcdd7f64f18137f5eebd672cf874
orenkash5 <orenkas@post.bgu.ac.il>
2026-04-18 11:09:52 +0300

    Add refund to PaymentService and create SupplyService

--- src/main/java/Infastructure/PaymentServiceMock.java
+++ src/main/java/Infastructure/PaymentServiceMock.java
@@ -6,10 +6,16 @@ public class PaymentServiceMock implements IPaymentService {
     
     @Override
     public boolean processPayment(String creditCardDetails, double amount) {
+        // check if the credit card details are valid and amount is positive
         if (creditCardDetails == null || creditCardDetails.trim().isEmpty() || amount <= 0) {
             return false;
         }
         return true; 
+    }
+
+    @Override
+    public boolean refund(String creditCardDetails, double amount) {
+        return true; // for simplicity, we assume all refunds are successful
     }
 
     @Override
