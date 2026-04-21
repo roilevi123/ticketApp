9796da83bf283f5bf3b02de55c3208918e7ca430
Ben Aronowitz <benaro@post.bgu.ac.il>
2026-04-21 20:39:14 +0300

    create action that update user personal details #13

--- src/main/java/Domain/User/IUserRepository.java
+++ src/main/java/Domain/User/IUserRepository.java
@@ -5,6 +5,7 @@ public interface IUserRepository {
     public String getUserPassword(String username);
     public User getUser(String username);
     public boolean userExists(String username);
+    public void save(User userToUpdate);
     public void deleteAll();
     public void deleteUser(String username);
 
