d73ea6e0e1e34deb3d8b9ac290b182dcacb06617
roilevi05 <roi9@post.bgu.ac.il>
2026-05-07 10:04:30 +0300

    fix

--- src/main/java/Appliction/QueueService.java
+++ src/main/java/Appliction/QueueService.java
@@ -1,17 +1,25 @@
 package Appliction;
 
-import Domain.Domains.QueueDomain;
 import Domain.QueueAggregates.QueueEntry;
-
+import Domain.QueueAggregates.iQueueRepository;
 import java.util.List;
 import java.util.Optional;
 
 public class QueueService {
-    private QueueDomain domain;
-    public QueueService(QueueDomain domain) {
-        this.domain = domain;
+    private iQueueRepository queueRepository;
+    private final int MAX_ACTIVE_USERS = 100;
+    private final long ACCESS_DURATION = 10 * 60 * 1000;
+
+    public QueueService(iQueueRepository queueRepository) {
+        this.queueRepository = queueRepository;
     }
+
     public String checkStatus(String eventId, String username) {
-        return domain.checkStatus(eventId, username);
+        return queueRepository.checkStatusAtomic(
+                eventId,
+                username,
+                MAX_ACTIVE_USERS,
+                ACCESS_DURATION
+        );
     }
 }
