30b7a3ccf679bf3ae11c78480624e23c37bcc3c0
Tevel Schlezinger <tevelschlez@gmail.com>
2026-05-25 23:42:47 +0300

    user repository: changed the id of the map to be string

--- src/main/java/com/ticketing/ticketapp/Domain/User/IUserRepository.java
+++ src/main/java/com/ticketing/ticketapp/Domain/User/IUserRepository.java
@@ -13,6 +13,6 @@ public interface IUserRepository {
     public void save(User userToUpdate);
     public void deleteAll();
     public void deleteUser(String ID);
-    public Map<Integer, Suspension> currentSuspensions();
+    public Map<String, Suspension> currentSuspensions();
     public List<Suspension> suspensionsHistory();
 }
