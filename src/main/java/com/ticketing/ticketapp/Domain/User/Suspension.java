cef2055099b0b0c2735e63f820ce64ae1070d5f4
Tevel Schlezinger <tevelschlez@gmail.com>
2026-05-25 23:25:55 +0300

    Suspension class: added fields

--- src/main/java/com/ticketing/ticketapp/Domain/User/Suspension.java
+++ src/main/java/com/ticketing/ticketapp/Domain/User/Suspension.java
@@ -1,4 +1,9 @@
 package com.ticketing.ticketapp.Domain.User;
 
+import java.time.LocalDateTime;
+
 public class Suspension {
+    int userID;
+    LocalDateTime startTime;
+    LocalDateTime endTime;
 }
