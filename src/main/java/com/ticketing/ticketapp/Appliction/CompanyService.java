b3c75f3b268331aa6756c25aa9ab90fdfb5d80d9
Tevel Schlezinger <tevelschlez@gmail.com>
2026-05-26 00:47:18 +0300

    added checks fot the freeze company functionality

--- src/main/java/com/ticketing/ticketapp/Appliction/CompanyService.java
+++ src/main/java/com/ticketing/ticketapp/Appliction/CompanyService.java
@@ -284,6 +284,8 @@ public class CompanyService {
             }
             String username = tokenService.extractUsername(token);
             logger.info("trying freeze company", username, company);
+            if(userRepository.isUserSuspendedNow(username))
+                throw new RuntimeException("User is suspended");
             Company companyObj = companyRepository.getCompany(company);
             companyObj.freezeCompany(username);
             companyRepository.save(companyObj);
