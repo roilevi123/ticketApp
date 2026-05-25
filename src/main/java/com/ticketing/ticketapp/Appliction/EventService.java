fb1dfb56f81216511548167b9f235871e11a2086
Tevel Schlezinger <tevelschlez@gmail.com>
2026-05-26 00:54:44 +0300

    added checks for the update event functionality

--- src/main/java/com/ticketing/ticketapp/Appliction/EventService.java
+++ src/main/java/com/ticketing/ticketapp/Appliction/EventService.java
@@ -124,6 +124,9 @@ public class EventService {
             if (username == null || !isAuthorized(company, username)) {
                 throw new RuntimeException("Unauthorized: User is not an owner or authorized manager");
             }
+
+            if(userRepository.isUserSuspendedNow(username))
+                throw new RuntimeException("User is suspended");
             Event event = eventRepository.getEvent(eventName, company);
             if (event == null) {
                 throw new RuntimeException("Event not found: " + eventName);
