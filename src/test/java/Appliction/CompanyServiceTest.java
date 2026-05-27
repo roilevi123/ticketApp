e65a4ea4c275df0c5bc511634b9956e2631d4560
Tevel Schlezinger <tevelschlez@gmail.com>
2026-05-27 17:37:38 +0300

    testing: added unit test for fire owner as suspended user

--- src/test/java/Appliction/CompanyServiceTest.java
+++ src/test/java/Appliction/CompanyServiceTest.java
@@ -842,4 +842,24 @@ class CompanyServiceTest {
         verify(treeOfRoleRepository, never()).save(any(Manager.class));
         assertFalse(mockManager.isAccepted());
     }
+
+    @Test
+    void FireOwner_Failure_SuspendedUser() {
+        String ownerToFire = "owner_to_fire";
+        String mockUserId = "user-123";
+
+        when(tokenService.validateToken(TOKEN)).thenReturn(true);
+        when(tokenService.extractUsername(TOKEN)).thenReturn(USERNAME);
+        when(tokenService.extractUserId(TOKEN)).thenReturn(mockUserId);
+
+        when(userRepository.isUserSuspendedNow(mockUserId)).thenReturn(true);
+
+        Response<String> res = companyService.FireOwner(TOKEN, COMPANY, ownerToFire);
+
+        assertFalse(res.isSuccess());
+        assertTrue(res.isError());
+        assertEquals("User is suspended", res.getMessage());
+
+        verify(treeOfRoleRepository, never()).deleteOwner(ownerToFire, COMPANY);
+    }
 }
