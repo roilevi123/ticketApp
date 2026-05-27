171ceb39026c7ae4446c50ccb5f3dc18f996b217
Ben Aronowitz <benaro@post.bgu.ac.il>
2026-05-27 17:47:52 +0300

    small conflict fix

--- src/main/java/com/ticketing/ticketapp/Appliction/IPendingNotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ticketing.ticketapp.Appliction;
-
-import java.util.List;
-
-public interface IPendingNotificationRepository {
-    void save(String userId, String message);
-    List<String> retrieveAndDelete(String userId);
-}
