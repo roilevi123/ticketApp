86740b1ad36c88b29eab7032811b054e83e07078
Tevel Schlezinger <tevelschlez@gmail.com>
2026-06-13 15:09:20 +0300

    logs in queue service

--- src/main/java/com/ticketing/ticketapp/Appliction/QueueService.java
+++ src/main/java/com/ticketing/ticketapp/Appliction/QueueService.java
@@ -4,12 +4,16 @@ import com.ticketing.ticketapp.Domain.AdminAggregate.iAdminRepository;
 import com.ticketing.ticketapp.Domain.QueueAggregates.QueueEntry;
 import com.ticketing.ticketapp.Domain.QueueAggregates.iQueueRepository;
 import com.ticketing.ticketapp.Infastructure.TokenService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Service
 public class QueueService {
     private iQueueRepository queueRepository;
@@ -19,6 +23,8 @@ public class QueueService {
     private final int MAX_ACTIVE_USERS = 100;
     private final long ACCESS_DURATION = 10 * 60 * 1000;
     private final Map<String, Integer> eventFlowRates = new ConcurrentHashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(PurchasePolicyService.class);
+
 
     public QueueService(iQueueRepository queueRepository, TokenService tokenService, INotifier notifier, iAdminRepository adminRepository) {
         this.queueRepository = queueRepository;
@@ -29,6 +35,7 @@ public class QueueService {
 
     public Response<String> checkStatus(String token, String eventId) {
         try {
+            logger.info("User of token {} is attempting to check status for the event: {}", token, eventId);
             if (!tokenService.validateToken(token)) {
                 return Response.error("Invalid token");
             }
@@ -48,6 +55,7 @@ public class QueueService {
                         "You've been granted access to purchase tickets for event '" + eventId + "'. Act fast before your time expires!");
             }
 
+            logger.info("User {} checked status for event {} successfully", userID, eventId);
             return Response.success(status);
         } catch (Exception e) {
             return Response.error(e.getMessage());
@@ -56,10 +64,12 @@ public class QueueService {
 
     public Response<List<QueueEntry>> getQueueForAdmin(String adminId, String eventId) {
         try {
+            logger.info("Admin {} is attempting to get queue for the event: {}", adminId, eventId);
             if (!adminRepository.isAdmin(adminId)) {
                 return Response.error("Admin does not exist");
             }
             List<QueueEntry> queue = queueRepository.getQueue(eventId);
+            logger.info("Admin {} got queue for event {} successfully", adminId, eventId);
             return Response.success(queue);
         } catch (Exception e) {
             return Response.error(e.getMessage());
@@ -68,10 +78,12 @@ public class QueueService {
 
     public Response<String> clearQueueForAdmin(String adminId, String eventId) {
         try {
+            logger.info("Admin {} is attempting to clear queue for the event: {}", adminId, eventId);
             if (!adminRepository.isAdmin(adminId)) {
                 return Response.error("Admin does not exist");
             }
             queueRepository.clearQueue(eventId);
+            logger.info("Admin {} cleared queue for event {} successfully", adminId, eventId);
             return Response.success("Queue cleared for event: " + eventId);
         } catch (Exception e) {
             return Response.error(e.getMessage());
@@ -80,6 +92,7 @@ public class QueueService {
 
     public Response<String> setFlowRate(String adminId, String eventId, int maxActiveUsers) {
         try {
+            logger.info("Admin {} is attempting to set flow rate (max active users: {}) the event: {}", adminId,maxActiveUsers, eventId);
             if (!adminRepository.isAdmin(adminId)) {
                 return Response.error("Admin does not exist");
             }
@@ -87,6 +100,7 @@ public class QueueService {
                 return Response.error("maxActiveUsers must be at least 1");
             }
             eventFlowRates.put(eventId, maxActiveUsers);
+            logger.info("Admin {} set flow rate (max active users: {}) for event {} successfully", adminId,maxActiveUsers, eventId);
             return Response.success("Flow rate set to " + maxActiveUsers + " for event: " + eventId);
         } catch (Exception e) {
             return Response.error(e.getMessage());
