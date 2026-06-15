1c30d36498f3dab8a80cf160df2c3244c4a836fe
Tevel Schlezinger <tevelschlez@gmail.com>
2026-06-16 01:15:18 +0300

    defined repository

--- src/main/java/com/ticketing/ticketapp/Infastructure/JpaOrderRepository.java
+++ src/main/java/com/ticketing/ticketapp/Infastructure/JpaOrderRepository.java
@@ -1,4 +1,15 @@
 package com.ticketing.ticketapp.Infastructure;
 
-public interface JpaOrderRepository {
+import com.ticketing.ticketapp.Domain.Order.ActiveOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface JpaOrderRepository extends JpaRepository<ActiveOrder, String>{
 }
