ac23070a44c0bbaa2b1c7a7eb1c06a1f5ab20dae
roilevi123 <roi9@post.bgu.ac.il>
2026-06-19 02:06:26 +0300

    final

--- src/test/java/AcceptanceTests/InformationEventsDbTests.java
+++ src/test/java/AcceptanceTests/InformationEventsDbTests.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.TestPropertySource;
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
         "repository.type=DB",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class InformationEventsDbTests extends InformationEventsTestsBase {
 }
