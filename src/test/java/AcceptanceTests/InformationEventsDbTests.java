56a1ada9045d1c9e7dc1adcdf970052d9814c572
roilevi123 <roi9@post.bgu.ac.il>
2026-06-19 08:51:19 +0300

    final

--- src/test/java/AcceptanceTests/InformationEventsDbTests.java
+++ src/test/java/AcceptanceTests/InformationEventsDbTests.java
@@ -6,6 +6,12 @@ import org.springframework.test.context.TestPropertySource;
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
         "repository.type=DB",
+        "spring.datasource.url=jdbc:postgresql://136.115.146.17:5432/ticketapp_test_db",
+        "spring.datasource.username=ticketapp_user",
+        "spring.datasource.password=BGUticketapp1!",
+        "spring.datasource.driver-class-name=org.postgresql.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class InformationEventsDbTests extends InformationEventsTestsBase {
 }
--- src/test/java/AcceptanceTests/WaitingQueueDbTests.java
+++ src/test/java/AcceptanceTests/WaitingQueueDbTests.java
@@ -5,7 +5,13 @@ import org.springframework.test.context.TestPropertySource;
 
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
-        "repository.type=DB"
+        "repository.type=DB",
+        "spring.datasource.url=jdbc:postgresql://136.115.146.17:5432/ticketapp_test_db",
+        "spring.datasource.username=ticketapp_user",
+        "spring.datasource.password=BGUticketapp1!",
+        "spring.datasource.driver-class-name=org.postgresql.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 public class WaitingQueueDbTests extends WaitingQueueTestsBase {
 }
