a4bcf4b54809b50edd49cb869bbb27b82b9bee2b
roilevi123 <roi9@post.bgu.ac.il>
2026-06-19 09:44:47 +0300

    final

--- src/test/java/AcceptanceTests/PurchaseOrderDbTests.java
+++ src/test/java/AcceptanceTests/PurchaseOrderDbTests.java
@@ -6,12 +6,14 @@ import org.springframework.test.context.TestPropertySource;
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
         "repository.type=DB",
-        "spring.datasource.url=jdbc:postgresql://136.115.146.17:5432/ticketapp_test_db",
-        "spring.datasource.username=ticketapp_user",
-        "spring.datasource.password=BGUticketapp1!",
-        "spring.datasource.driver-class-name=org.postgresql.Driver",
-        "spring.jpa.database-platform=org.hibernate.dialect.PostgreSQLDialect",
-        "spring.jpa.hibernate.ddl-auto=update"
+
+        "spring.datasource.url=jdbc:h2:mem:ticketapp_testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 public class PurchaseOrderDbTests extends PurchaseOrderTestsBase {
 }
